0fa7cafb7cc6522d5bc7bc9c427d8deafea5ab8a
tokenbrice <me@tokenbrice.com>
2026-04-26 21:25:04 +0200

    Recenter village landmarks

--- claudeville/src/config/townPlan.js
+++ claudeville/src/config/townPlan.js
@@ -4,10 +4,10 @@
 
 export const TOWN_DISTRICTS = Object.freeze([
     { id: 'civic', label: 'City Center', center: [20, 20], radius: 7 },
-    { id: 'knowledge', label: 'Upper Periphery', center: [12, 10], radius: 6 },
+    { id: 'knowledge', label: 'Upper Periphery', center: [14, 13], radius: 5 },
     { id: 'workshop', label: 'Forge Row', center: [30, 26], radius: 5 },
-    { id: 'resource', label: 'Mine Yard', center: [7, 31], radius: 5 },
-    { id: 'arcane', label: 'Portal Periphery', center: [10, 27], radius: 6 },
+    { id: 'resource', label: 'Mine Yard', center: [18, 26], radius: 4 },
+    { id: 'arcane', label: 'Portal Periphery', center: [14, 23], radius: 4 },
     { id: 'harbor', label: 'Harbor Quay', center: [36, 19], radius: 5 },
 ]);
 
@@ -34,7 +34,7 @@ export const TOWN_ROAD_ROUTES = Object.freeze([
         id: 'observatory-road',
         material: 'dirt',
         width: 1,
-        points: [[17, 19], [15, 15], [13, 12], [11, 11]],
+        points: [[17, 19], [16, 16], [14, 14]],
     },
     {
         id: 'harbor-quay',
@@ -52,13 +52,13 @@ export const TOWN_ROAD_ROUTES = Object.freeze([
         id: 'portal-road',
         material: 'dirt',
         width: 1,
-        points: [[16, 22], [13, 24], [10, 26]],
+        points: [[16, 22]],
     },
     {
         id: 'mine-road',
         material: 'dirt',
         width: 1,
-        points: [[10, 26], [9, 29], [7, 30]],
+        points: [[18, 23], [18, 25]],
     },
     {
         id: 'forge-road',
